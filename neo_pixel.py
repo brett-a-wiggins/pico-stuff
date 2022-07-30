b3f881ef061483e9b245bc9124a2d5abc2a7f02b
Brett Wiggins <brett@criticalanalysis.com.au>
2022-07-30 11:15:26 +1000

    third commit

--- neo_pixel.py
+++ neo_pixel.py
@@ -3,28 +3,45 @@ import neopixel
 import time
 from adafruit_led_animation.animation.blink import Blink
 from adafruit_led_animation.sequence import AnimationSequence
-from adafruit_led_animation.color import RED
+from adafruit_led_animation.color import RED, GREEN, BLACK
 
+num_pixels = 5
+pixels = neopixel.NeoPixel(board.GP0, num_pixels, brightness=0.1)
 
-    
-def health_meter(health):
-    num_pixels = health
-    pixels = neopixel.NeoPixel(board.GP0, num_pixels, brightness=0.1)
+def pixel_fill(colour, size):
+    index = 0
+    while index < size:
+        pixels[index] = colour
+        index += 1
+    while index < 5:
+        pixels[index] = BLACK
+        index += 1
+
+def display_health(colour, level, delay=0.0):
+    print("display_health(): " + str(colour) + " " + level + " " + delay)
+#   print("display_health(): {0} {1} {2}".format(colour, level, delay))
     pixels.brightness = 0.5
-    pixels.fill((0,255,0))
-    time.sleep(3)
+    pixels.fill(colour)
+#   pixel_fill(colour, level)
+    time.sleep(delay)
+
+"""
     blink = Blink(pixels, speed=1, color=RED)
-    animations = AnimationSequence(
-    blink,
-    auto_clear=True)
+    animations = AnimationSequence(blink, auto_clear=True)
     animations.animate()
-    time.sleep(0.3)
-    pixels.deinit()
-    return health - 1
+    time.sleep(delay)
+"""
 
-health = 5
-health = health_meter(health)
-health = health_meter(health)
+# TODO: Make sure health level > zero
+def take_damage(level, damage):
+    print("take_damage(): {0} {1}".format(level, damage))
+    display_health(RED, level, 0.5)
+    level = level - damage
+    display_health(GREEN, level)
+    return level
 
-
-
+health_level = 5
+display_health(BLACK, health_level, 0.5)
+display_health(GREEN, health_level, 0.5)
+# TODO: while loop !
+health_level = take_damage(health_level, 1)
